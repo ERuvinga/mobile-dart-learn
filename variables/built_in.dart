9e4b232854fd3db6973bb0944739327be5517be6
ERuvinga <ruvingaelie@gmail.com>
2024-01-07 21:28:41 +0200

    Adding variables type: numbers, String, and collections

---
+++ variables/built_in.dart
@@ -0,0 +1,17 @@
+// file for variables
+
+void main() {
+  // varaibles
+
+  int numbrValue = 2; // number
+  String name = "Elie";
+
+  //constantes
+  const bool majeur = true;
+  final int age = 21;
+
+  //collections
+  List<String> names = ["Kasereka", "Ruvinga", "Elie"]; // les tableaux
+
+  print("${names[2]}");
+}
